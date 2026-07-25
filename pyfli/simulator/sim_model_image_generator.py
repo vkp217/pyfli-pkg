a7630076a7f271359989a05085a539f96fc97766
Vikas Pandey <vikaspvaletude@gmail.com>
2026-07-25 13:02:27 -0400

    Format sim_model_image_generator.py with ruff

--- pyfli/simulator/sim_model_image_generator.py
+++ pyfli/simulator/sim_model_image_generator.py
@@ -27,16 +27,17 @@ class FLIModelImageGenerator:
     cubes and parameter maps.
     """
 
-    def __init__(self,
-                 irf_data,
-                 intensity_image_path=None,
-                 roi_mask_path=None,
-                 roi_params=None,
-                 image_shape=(32, 32),
-                 method='ICCD',
-                 verbose=True,
-                 bool_mask=None
-                 ):
+    def __init__(
+        self,
+        irf_data,
+        intensity_image_path=None,
+        roi_mask_path=None,
+        roi_params=None,
+        image_shape=(32, 32),
+        method="ICCD",
+        verbose=True,
+        bool_mask=None,
+    ):
         """Loads masks and instantiates one simulator per ROI.
 
         Args:
@@ -73,11 +74,13 @@ class FLIModelImageGenerator:
         self.method = method.lower()
         self.irf_data = irf_data
         self.verbose = verbose
-        self.bool_mask = np.asarray(bool_mask, dtype=bool) if bool_mask is not None else None
-        
+        self.bool_mask = (
+            np.asarray(bool_mask, dtype=bool) if bool_mask is not None else None
+        )
+
         # Loading the intensity Mask
         if intensity_image_path:
-            img = Image.open(intensity_image_path).convert('L')
+            img = Image.open(intensity_image_path).convert("L")
             self.intensity_mask = np.array(img).astype(float) / 255.0
             self.shape = self.intensity_mask.shape
         else:
@@ -86,9 +89,10 @@ class FLIModelImageGenerator:
 
         # loading the ROI Mask (multi-cluster mask)
         if roi_mask_path:
-            mask_img = Image.open(roi_mask_path).convert('L')
-            self.roi_mask = np.array(mask_img.resize((self.shape[1], self.shape[0]), 
-                                     Image.NEAREST)).astype(int)
+            mask_img = Image.open(roi_mask_path).convert("L")
+            self.roi_mask = np.array(
+                mask_img.resize((self.shape[1], self.shape[0]), Image.NEAREST)
+            ).astype(int)
         else:
             self.roi_mask = np.zeros(self.shape, dtype=int)
 
@@ -97,13 +101,19 @@ class FLIModelImageGenerator:
         # dummy_irf = irf_data[0, 0, :] if irf_data.ndim == 3 else irf_data
         self.roi_sims = {}
         unique_rois = np.unique(self.roi_mask)
-        SimClass = ContinuousSimulator if self.method == 'iccd' else PhotonCountSimulator
+        SimClass = (
+            ContinuousSimulator if self.method == "iccd" else PhotonCountSimulator
+        )
 
         for roi_val in unique_rois:
-            cfg = roi_params[roi_val].copy() if (roi_params and roi_val < len(roi_params)) else {}
-            default_sensor = 'ICCD' if self.method == 'iccd' else 'PHOTON_COUNTER'
-            sensor_type = cfg.pop('sensor_type', default_sensor)
-            cfg.pop('method', None) 
+            cfg = (
+                roi_params[roi_val].copy()
+                if (roi_params and roi_val < len(roi_params))
+                else {}
+            )
+            default_sensor = "ICCD" if self.method == "iccd" else "PHOTON_COUNTER"
+            sensor_type = cfg.pop("sensor_type", default_sensor)
+            cfg.pop("method", None)
             self.roi_sims[roi_val] = SimClass(dummy_irf, sensor_type=sensor_type, **cfg)
 
     def generate_image(self):
@@ -127,17 +137,17 @@ class FLIModelImageGenerator:
         """
         h, w = self.shape
         total_pixels = h * w
-        
+
         # determining time-axis length
         first_roi = next(iter(self.roi_sims))
         sample = self.roi_sims[first_roi]()
         t_len = sample["raw_data"]["decay"].size
-        
+
         # Pre-allocate
         decay_cube = np.zeros((h, w, t_len), dtype=np.float32)
         fit_cube = np.zeros((h, w, t_len), dtype=np.float32)
         irf_cube = np.zeros((h, w, t_len), dtype=np.float32)
-        
+
         param_keys = sample["results"]["maps"].keys()
         param_maps = {k: np.zeros((h, w), dtype=np.float32) for k in param_keys}
 
@@ -145,17 +155,19 @@ class FLIModelImageGenerator:
             print(f"Generating {self.method.upper()} FLI Image [{h}x{w}x{t_len}]...")
 
         pixel_iterator = itertools.product(range(h), range(w))
-        
+
         # --- tqdm OUTSIDE THE LOOP ---
-        with tqdm(total=total_pixels, 
-                  desc="Simulating Pixels", 
-                  unit="px",
-                  disable=not self.verbose,
-                  leave=False) as pbar:
+        with tqdm(
+            total=total_pixels,
+            desc="Simulating Pixels",
+            unit="px",
+            disable=not self.verbose,
+            leave=False,
+        ) as pbar:
             for i, j in pixel_iterator:
                 roi_val = self.roi_mask[i, j]
                 sim = self.roi_sims[roi_val]
-                
+
                 # Pixel-wise IRF handling
                 if self.irf_data.ndim == 3:
                     current_irf = self.irf_data[i, j, :]
@@ -163,38 +175,40 @@ class FLIModelImageGenerator:
                     norm_irf = current_irf / irf_sum if irf_sum > 0 else current_irf
                     sim.engine.irf = norm_irf
                 else:
-                    norm_irf = sim.engine.irf 
-                
+                    norm_irf = sim.engine.irf
+
                 # Run Simulation
                 pixel_data = sim()
                 m = self.intensity_mask[i, j]
-                
+
                 decay_cube[i, j, :] = pixel_data["raw_data"]["decay"] * m
                 fit_cube[i, j, :] = pixel_data["results"]["TR_maps"]["fit_map"] * m
-                irf_cube[i, j, :] = norm_irf 
-                
+                irf_cube[i, j, :] = norm_irf
+
                 for k in param_keys:
                     param_maps[k][i, j] = pixel_data["results"]["maps"][k]
-                
+
                 # Manually update the bar
                 pbar.update(1)
-        
+
         if self.bool_mask is not None:
             if self.bool_mask.shape != (h, w):
                 raise ValueError(
                     f"bool_mask shape {self.bool_mask.shape} does not match image shape {(h, w)}."
                 )
-            m3 = self.bool_mask[:, :, np.newaxis]   # (H, W, 1) for broadcasting over time axis
-            decay_cube   = decay_cube   * m3
-            fit_cube     = fit_cube     * m3
-            irf_cube     = irf_cube     * m3
+            m3 = self.bool_mask[
+                :, :, np.newaxis
+            ]  # (H, W, 1) for broadcasting over time axis
+            decay_cube = decay_cube * m3
+            fit_cube = fit_cube * m3
+            irf_cube = irf_cube * m3
             for k in param_maps:
                 param_maps[k] = param_maps[k] * self.bool_mask
 
         return {
-            "raw_data": {"decay": decay_cube,
-                         "irf": irf_cube},
-            "results": {"maps": param_maps,
-                        "TR_maps": {"fit_map": fit_cube,
-                        "residual_map": decay_cube - fit_cube}}
-                    }
+            "raw_data": {"decay": decay_cube, "irf": irf_cube},
+            "results": {
+                "maps": param_maps,
+                "TR_maps": {"fit_map": fit_cube, "residual_map": decay_cube - fit_cube},
+            },
+        }
